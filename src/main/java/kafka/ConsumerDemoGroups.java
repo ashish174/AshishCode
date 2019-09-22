168ebc26498edef87684c02062e230b29159fefb
AshishKumar <dhruva.ashu@gmail.com>
2019-09-22 12:08:41 +0530

    Kafka

---
+++ src/main/java/kafka/ConsumerDemoGroups.java
@@ -0,0 +1,44 @@
+package kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemoGroups {
+    private static Logger logger = LoggerFactory.getLogger(ConsumerDemoGroups.class);
+    private static String group = "my_application2";
+    private static String topic = "first_topic";
+
+    public static void main(String[] args) {
+        //Create Consumer Config
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, group);
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        //Create Consumer
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(properties);
+
+        //subscribe to topic
+        kafkaConsumer.subscribe(Arrays.asList(topic));
+
+        //poll for msg
+        while(true){
+            ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(100));
+            for(ConsumerRecord consumerRecord : consumerRecords){
+                logger.info("Key : {} , Value : {}", consumerRecord.key(), consumerRecord.value());
+                logger.info("Partition {}, Offset {}", consumerRecord.partition(), consumerRecord.offset());
+            }
+        }
+
+    }
+}
--- src/main/java/miscellenous/EnumVerify.java
+++ src/main/java/miscellenous/EnumVerify.java
@@ -1,14 +1,32 @@
 package miscellenous;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.*;
 
 public class EnumVerify {
+  Logger logger = LoggerFactory.getLogger(EnumVerify.class);
+
+
   public static void main(String[] args) {
-    Date CREATEDATE = new Date();
+
+    //animal.boo();
+      System.out.println(LicenseType.UNKNOWN==null);
+
+    /*Date CREATEDATE = new Date();
     System.out.println(CREATEDATE);
 
+
+    System.out.println("Result"+"Ashis".equals(null));
     List<String> stringList = new ArrayList<>();
+    List<Long> longList = new ArrayList<>();
     System.out.println(stringList.isEmpty());
+    stringList.add("abc");
+    stringList.add("bc");
+    longList.add(1L);
+    longList.add(2L);
+    System.out.println("List: "+longList);
     System.out.println(ReportGenerationStrategy.valueOf(1).name());
     String licenseType = ReportGenerationStrategy.DEVICE.toString();
     System.out.println(ReportGenerationStrategy.valueOf("USER").name());
@@ -18,9 +36,23 @@ public class EnumVerify {
     System.out.println(ReportGenerationStrategy.USER);
     System.out.println("Device Type is "+ReportGenerationStrategy.DEVICE);
     System.out.println(UUID.randomUUID());
-    System.out.println(UUID.randomUUID().toString().replace("-", ""));
+    System.out.println(UUID.randomUUID().toString().replace("-", ""));*/
 
   }
+}
+ enum LicenseType {
+    DEVICE(0), USER(1), UNKNOWN(null);
+    private Integer value;
+    private static Map map = new HashMap<>();
+
+
+    LicenseType(Integer i) {
+        this.value = i;
+    }
+
+    public Integer getValue(){
+        return value;
+    }
 }
 
  enum ReportGenerationStrategy {
