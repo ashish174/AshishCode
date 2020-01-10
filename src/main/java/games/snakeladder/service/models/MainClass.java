449a4893f1e30892ae9de50f8552fd9615c6963d
AshishKumar <dhruva.ashu@gmail.com>
2020-01-10 20:26:26 +0530

    chnge

--- src/main/java/games/snakeladder/service/models/MainClass.java
+++ src/main/java/games/snakeladder/service/models/MainClass.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class MainClass {
     public static void main(String[] args) {
         int gameSize = 50;
-        Player player1 = new Player("Ashish");
-        Player player2 = new Player("Anshu");
-        Player player3 = new Player("Jhumi");
+        Player player1 = new Player("Ashish", null);
+        Player player2 = new Player("Anshu", null);
+        Player player3 = new Player("Jhumi", null);
 
         List<Player> playerList = new ArrayList<>();
         playerList.add(player1);
