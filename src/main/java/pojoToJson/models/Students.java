b49faf6ee367652628c5a0fe5ff9d5ccc12a8a59
AshishKumar <dhruva.ashu@gmail.com>
2020-04-29 16:38:12 +0530

    mapper

---
+++ src/main/java/pojoToJson/models/Students.java
@@ -0,0 +1,15 @@
+package pojoToJson.models;
+
+import java.util.List;
+
+public class Students{
+  List<Student> students;
+
+  public List<Student> getStudents() {
+    return students;
+  }
+
+  public void setStudents(List<Student> students) {
+    this.students = students;
+  }
+}
