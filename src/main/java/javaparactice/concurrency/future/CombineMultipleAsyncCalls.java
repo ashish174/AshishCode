347534fc759c6b131ccbbb28ecbbf595d6abafdb
AshishKumar <dhruva.ashu@gmail.com>
2020-08-09 10:18:50 +0530

    Async

---
+++ src/main/java/javaparactice/concurrency/future/CombineMultipleAsyncCalls.java
@@ -0,0 +1,9 @@
+package javaparactice.concurrency.future;
+
+import java.util.concurrent.CompletableFuture;
+
+public class CombineMultipleAsyncCalls {
+    /*CompletableFuture<String> completableFuture = CompletableFuture
+            .*/
+
+}
