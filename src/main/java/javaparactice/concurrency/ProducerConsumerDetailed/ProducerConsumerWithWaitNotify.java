a645e82aa43789f14bf5abe243729a0dedf463a0
AshishKumar <dhruva.ashu@gmail.com>
2021-04-26 11:46:28 +0530

    Producer consumer

--- src/main/java/javaparactice/concurrency/ProducerConsumerDetailed/ProducerConsumerWithWaitNotify.java
+++ src/main/java/javaparactice/concurrency/ProducerConsumerDetailed/ProducerConsumerWithWaitNotify.java
@@ -8,9 +8,12 @@ public class ProducerConsumerWithWaitNotify {
     final Integer max_Size = 5;
     List<Integer> sharedTaskList = new ArrayList<>(max_Size);
     //Queue<Integer> sharedQueue = new LinkedList<Integer>(5);
-    Thread producerThread = new Thread(new Producer(sharedTaskList, max_Size), "Producer");
+    //2 Producer Thread and 1 Consumer Thread
+    Thread producerThread1 = new Thread(new Producer(sharedTaskList, max_Size), "Producer1");
+    Thread producerThread2 = new Thread(new Producer(sharedTaskList, max_Size), "Producer2");
     Thread consumerThread = new Thread(new Consumer(sharedTaskList, max_Size), "Consumer");
-    producerThread.start();
+    producerThread1.start();
+    producerThread2.start();
     consumerThread.start();
   }
 }
