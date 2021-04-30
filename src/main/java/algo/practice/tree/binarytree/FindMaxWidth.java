90831cad3490098d63299740104c3151a96d4d49
AshishKumar <dhruva.ashu@gmail.com>
2021-04-30 11:20:13 +0530

    max width

---
+++ src/main/java/algo/practice/tree/binarytree/FindMaxWidth.java
@@ -0,0 +1,47 @@
+package algo.practice.tree.binarytree;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FindMaxWidth {
+    public static int maxWidth = 0;
+    public static int level = 0;
+
+    public static int findWidth(Node root){
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            level++;
+            int levelWidth = queue.size();
+            System.out.println("TreeWidth at level "+level+" : "+levelWidth);
+            maxWidth = Math.max(maxWidth, levelWidth);
+            for(int i=0; i < levelWidth; i++){
+                Node node = queue.remove();
+                if(node.left!=null){
+                    queue.add(node.left);
+                }
+                if(node.right!=null){
+                    queue.add(node.right);
+                }
+            }
+        }
+        return maxWidth;
+    }
+
+    public static int findWidthRecursively(Node root, int level){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right = new Node(3);
+        root.right.right = new Node(8);
+        root.right.right.left = new Node(6);
+        root.right.right.right = new Node(7);
+        System.out.println("Tree Width : "+findWidth(root));
+    }
+}
