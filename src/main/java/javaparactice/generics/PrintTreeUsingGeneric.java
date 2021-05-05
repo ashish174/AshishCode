b09fc6b863f43aa681e72661fe33a389856dabff
AshishKumar <dhruva.ashu@gmail.com>
2021-05-05 12:28:10 +0530

    generics

---
+++ src/main/java/javaparactice/generics/PrintTreeUsingGeneric.java
@@ -0,0 +1,106 @@
+package javaparactice.generics;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PrintTreeUsingGeneric {
+
+    public static <T extends Comparable<?>> void printNode(GNode<T> root) {
+        int maxLevel = maxLevel(root);
+
+        printNodeInternal(Collections.singletonList(root), 1, maxLevel);
+    }
+
+    private static <T extends Comparable<?>> void printNodeInternal(List<GNode<T>> nodes, int level, int maxLevel) {
+        if (nodes.isEmpty() || isAllElementsNull(nodes))
+            return;
+
+        int floor = maxLevel - level;
+        int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+        int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+        int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+
+        printWhitespaces(firstSpaces);
+
+        List<GNode<T>> newNodes = new ArrayList<GNode<T>>();
+        for (GNode<T> node : nodes) {
+            if (node != null) {
+                System.out.print(node.data);
+                newNodes.add(node.left);
+                newNodes.add(node.right);
+            } else {
+                newNodes.add(null);
+                newNodes.add(null);
+                System.out.print(" ");
+            }
+
+            printWhitespaces(betweenSpaces);
+        }
+        System.out.println();
+
+        for (int i = 1; i <= endgeLines; i++) {
+            for (int j = 0; j < nodes.size(); j++) {
+                printWhitespaces(firstSpaces - i);
+                if (nodes.get(j) == null) {
+                    printWhitespaces(endgeLines + endgeLines + i + 1);
+                    continue;
+                }
+
+                if (nodes.get(j).left != null)
+                    System.out.print("/");
+                else
+                    printWhitespaces(1);
+
+                printWhitespaces(i + i - 1);
+
+                if (nodes.get(j).right != null)
+                    System.out.print("\\");
+                else
+                    printWhitespaces(1);
+
+                printWhitespaces(endgeLines + endgeLines - i);
+            }
+
+            System.out.println();
+        }
+
+        printNodeInternal(newNodes, level + 1, maxLevel);
+    }
+
+    private static void printWhitespaces(int count) {
+        for (int i = 0; i < count; i++)
+            System.out.print(" ");
+    }
+
+    private static <T extends Comparable<?>> int maxLevel(GNode<T> node) {
+        if (node == null)
+            return 0;
+
+        return Math.max(maxLevel(node.left), maxLevel(node.right)) + 1;
+    }
+
+    private static <T> boolean isAllElementsNull(List<T> list) {
+        for (Object object : list) {
+            if (object != null)
+                return false;
+        }
+
+        return true;
+    }
+
+
+
+    public static void main(String[] args) {
+        GNode root = new GNode(8);
+        root.left = new GNode(3);
+        root.left.left = new GNode(1);
+        root.left.right = new GNode(6);
+        root.right = new GNode(10);
+        root.right.right = new GNode(14);
+        root.right.right.left = new GNode(13);
+        root.right.right.right = new GNode(16);
+
+        PrintTreeUsingGeneric.printNode(root);
+    }
+}
