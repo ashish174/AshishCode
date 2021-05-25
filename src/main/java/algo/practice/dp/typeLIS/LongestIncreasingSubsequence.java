af80a7eb362006fc7d39ca57f1bd360491c59903
AshishKumar <dhruva.ashu@gmail.com>
2021-05-25 08:16:22 +0530

    LIS

--- src/main/java/algo/practice/dp/typeLIS/LongestIncreasingSubsequence.java
+++ src/main/java/algo/practice/dp/typeLIS/LongestIncreasingSubsequence.java
@@ -1,8 +1,17 @@
 package algo.practice.dp.typeLIS;
 
-public class LongestIncreasingSubsequence {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-    int findLISLength(int[] nums){
+/**
+ * Optimal Substructure :
+ * L(i) = 1 + max( L(j) ) where 0 < j < i and arr[j] < arr[i]; or
+ * L(i) = 1, if no such j exists.
+ */
+public class LongestIncreasingSubsequence {
+    public static final Logger LOGGER = LoggerFactory.getLogger(LongestIncreasingSubsequence.class);
+
+    public static int findLISLength(int[] nums){
         if(nums.length<1){
             return 0;
         }
@@ -27,10 +36,32 @@ public class LongestIncreasingSubsequence {
     }
 
 
+    public static int findLISOptimized(int[] seq){
+        if(seq.length<1){
+            return 0;
+        }
+        int[] dp = new int[seq.length];
+        for(int i=0; i<seq.length; i++ ){
+            dp[i]=1;
+        }
+        int LIS = 1;
+        for(int i=1; i<seq.length;i++){
+            int maxLISSoFar = 1;
+            for(int j=i-1; j >= 0; j--){
+                if(seq[j] < seq[i]){
+                    dp[i] = Math.max(dp[i], 1+dp[j]);
+                }
+            }
+            LIS = Math.max(LIS, dp[i]);
+        }
+        return LIS;
+    }
+
+
 
     public static void main(String[] args) {
         int[] nums = new int[]{10,9,2,5,3,7,101,18};
-        System.out.println(new LongestIncreasingSubsequence().findLISLength(nums));
+        LOGGER.info("LIS for nums is of size : {}", findLISOptimized(nums));
     }
 
 
