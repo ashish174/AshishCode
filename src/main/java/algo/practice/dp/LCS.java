780aece996eaeaa05e158e6daa6a2e1c061196e2
AshishKumar <dhruva.ashu@gmail.com>
2021-05-27 08:42:52 +0530

    Print LCS

--- src/main/java/algo/practice/dp/LCS.java
+++ src/main/java/algo/practice/dp/LCS.java
@@ -90,9 +90,37 @@ public class LCS {
         }
     }
 
+    public static char[] printLCS(char[] fArr, char[] sArr, int[][] lcs) {
+        int i = fArr.length;
+        int j = sArr.length;
+        int lcsLength = lcs[i][j];
+        char[] lcsCharArr = new char[lcsLength];
+        while (lcs[i][j] != 0) {
+            int maxFromLeftOrTop = Math.max(lcs[i - 1][j], lcs[i][j - 1]);
+            if (lcs[i][j] > maxFromLeftOrTop) {
+                // This char is part of lcs. Include it & move diagonally up
+                lcsCharArr[--lcsLength] = fArr[i - 1];
+                //Move diagonally up
+                i--;
+                j--;
+            } else {
+                if (lcs[i][j] == lcs[i - 1][j]) {
+                    // move left
+                    i--;
+                } else {
+                    //move up
+                    j--;
+                }
+            }
+        }
+        return lcsCharArr;
+    }
+
     public static void main(String[] args) {
-        char[] firstArr = "ABCDGH".toCharArray();
-        char[] secondArr = "AEDFHR".toCharArray();
+        //char[] firstArr = "ABCDGH".toCharArray();
+        //char[] secondArr = "AEDFHR".toCharArray();
+        char[] firstArr = "AGGTAB".toCharArray();
+        char[] secondArr = "GXTXAYB".toCharArray();
         //Recursive approach
         int lcsLength = findLCSLengthRecursive(firstArr, secondArr, firstArr.length, secondArr.length);
         LOGGER.info("LCS lenght using Recursion for arrays {} & {} is : {}", firstArr, secondArr, lcsLength);
@@ -104,6 +132,8 @@ public class LCS {
         int lcslength2 = findLCSLengthUsingMemoization(firstArr, secondArr, firstArr.length, secondArr.length);
         LOGGER.info("LCS lenght for using memoization  arrays {} & {} is : {}", firstArr, secondArr, lcslength1);
         PrintArray.print2DSquareMatrix(memoization);
+        char[] lcsCharArr = printLCS(firstArr, secondArr, memoization);
+        LOGGER.info("LCS for using memoization  arrays {} & {} is : {} with length {}", firstArr, secondArr, lcsCharArr, lcslength1);
 
     }
 }
