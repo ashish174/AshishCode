3ffd22eaad4186763eed632d02944b41e24e6cb4
AshishKumar <dhruva.ashu@gmail.com>
2021-05-28 11:35:10 +0530

    appln

--- src/main/java/algo/practice/dp/EditDistance.java
+++ src/main/java/algo/practice/dp/EditDistance.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
  * You can use three action on character - to convert one String to other string : Insert, Remove, Replace
  * Convert into minimum steps
  * Appln:- display all the words in a dictionary that are near proximity to a given word or a incorrectly spelled word
+ * Also used in Lucene Search Engine. Lucene includes a feature to perform a fuzzy search based on edit distance.
+ * Apache Lucene is imported by several apps like Elastic Search, Apache Solr, Mongo DB, Open Search
  *
  *
  * E(m,n) = Edit distance for string/char of size m & size n
