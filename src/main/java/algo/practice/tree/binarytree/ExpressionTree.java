b78379892816ad6c8c75080fd75d96f7d01b19d9
AshishKumar <dhruva.ashu@gmail.com>
2021-08-19 08:29:21 +0530

    expression tree

--- src/main/java/algo/practice/tree/binarytree/ExpressionTree.java
+++ src/main/java/algo/practice/tree/binarytree/ExpressionTree.java
@@ -24,6 +24,17 @@ public class ExpressionTree {
 
         logger.info("Value of expression is {}", evaluateExpressionTree(root));
 
+        root = new Node("+");
+        root.left = new Node("*");
+        root.left.left = new Node("5");
+        root.left.right = new Node("4");
+        root.right = new Node("-");
+        root.right.left = new Node("100");
+        root.right.right = new Node("/");
+        root.right.right.left = new Node("20");
+        root.right.right.right = new Node("2");
+        logger.info("Value of expression is {}", evaluateExpressionTree(root));
+
 
     }
 
