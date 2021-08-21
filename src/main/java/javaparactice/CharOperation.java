4c65ccf1b0c7271c28541319bd24e58d4150bac1
AshishKumar <dhruva.ashu@gmail.com>
2021-08-21 08:58:31 +0530

    char ops

---
+++ src/main/java/javaparactice/CharOperation.java
@@ -0,0 +1,13 @@
+package javaparactice;
+
+public class CharOperation {
+    public static void main(String[] args) {
+        char c = 65;
+        char ch = 'c';
+        char ch2 = 'e';
+        int num = ch;
+        System.out.println(c);
+        System.out.println(num);
+        System.out.println(ch2-ch);
+    }
+}
