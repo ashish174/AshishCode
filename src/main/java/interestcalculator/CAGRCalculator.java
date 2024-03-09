4a3815e2970d7fa3c6e14fbc0d55bd058b01ac24
ashish174 <dhruva.ashu@gmail.com>
2024-03-09 23:47:56 +0530

    Squash test slave (#24)
    * JerseyClient
    
    * squashed first & second commit
    
    3rd + 4th commit squashed
    
    * Gas station prblm statement
    
    * CAGR changes
    
    * sample changes
    
    * cagr
    
    * cagr - 2

--- src/main/java/interestcalculator/CAGRCalculator.java
+++ src/main/java/interestcalculator/CAGRCalculator.java
@@ -13,17 +13,70 @@ public class CAGRCalculator {
 
 
     public static void main(String[] args) {
-        double monthlyOrYearlyDeposit = 10000;
-        boolean isMonthlyDeposit = true;
-        int numOfYears = 7;
-        double finalAmount = 1400000;
-        double cagr = calculateInterest(monthlyOrYearlyDeposit, finalAmount, numOfYears, isMonthlyDeposit);
-        LOGGER.info("Calculating:-");
-        LOGGER.info("Period {} FinalAmount {} for monthlyOrYearlyDeposit deposit {}, Total final value {}", numOfYears, finalAmount, monthlyOrYearlyDeposit, rupeeFormat.format(finalAmount));
-
+        // Calculate Final Amount based on SIP amount, CAGR and num of years for SIP
+        LOGGER.info("Calculate Final Amount based on SIP amount, CAGR and num of years for SIP");
+        double monthlyOrYearlyDeposit1 = 100000;
+        boolean isMonthlyDeposit1 = true;
+        int numOfYears1 = 10;
+        double cagr1 = 12;
+        double finalAmount1 = calculateFinalAmount(
+                monthlyOrYearlyDeposit1,
+                cagr1,
+                numOfYears1,
+                isMonthlyDeposit1);
     }
 
     /**
+     * Calculate Final Amount reached for monthlyOrYearlyAmount/SIP for r cagr in x numOfyears
+     * @param monthlyOrYearlyAmount
+     * @param cagr
+     * @param numOfYears
+     * @param isMonthlyDeposit
+     * @return
+     */
+    private static double calculateFinalAmount(double monthlyOrYearlyAmount, double cagr, int numOfYears, boolean isMonthlyDeposit) {
+        double finalAmountWithInterest = 0;
+        for (int i = 0; i < numOfYears; i++) {
+            double currYearInstallment = isMonthlyDeposit
+                    ? monthlyOrYearlyAmount * 12
+                    : monthlyOrYearlyAmount;
+            int currYearInstallmentTotalPeriod = numOfYears - i;
+            double currYearInstallmentFinalValueWithInterest = currYearInstallment * Math.pow((1 + cagr / 100.0), currYearInstallmentTotalPeriod);
+            finalAmountWithInterest += currYearInstallmentFinalValueWithInterest;
+            LOGGER.info("Year {}  Period {} Installment amount {} , Total Final Value {}", i + 1, currYearInstallmentTotalPeriod, currYearInstallment, rupeeFormat.format(currYearInstallmentFinalValueWithInterest));
+        }
+        LOGGER.info("SIP Amount {}, SIP Type {}, CAGR {}, NumOfYears {} -> \n \t \t \t Total FinalAmount {}"
+                , monthlyOrYearlyAmount
+                , isMonthlyDeposit?"MONTHLY":"YEARLY"
+                , cagr
+                , numOfYears
+                , rupeeFormat.format(finalAmountWithInterest));
+        return finalAmountWithInterest;
+    }
+
+    /**
+     *
+     * Calculate monthlyOrYearly SIP required to reach a final amount with r CAGR in x numOfyears
+     * @param finalAmount
+     * @param cagr
+     * @param numOfYears
+     * @param isMonthlyDeposit
+     * @return
+     */
+    private static double calculateSIP(double finalAmount, double cagr, int numOfYears, boolean isMonthlyDeposit) {
+        // Summation is of form Geometric Series : ar^0 + ar^1 + ar^2 + ar^3 + ...ar^n-1 = a(1-r^n)/(1-r)
+        double finalAmountWithInterest = 0;
+        for (int i = 0; i < numOfYears; i++) {
+            // For each year
+            // currYearInstallmentTotalPeriod = numOfYears - i
+            // A = P * Math.pow((1 + rate / 100.0), currYearInstallmentTotalPeriod)
+        }
+        return 0;
+    }
+
+    /**
+     * This is a wrong implementation
+     *
      * Calculate CAGR required for monthlyOrYearlyAmount/SIP to reach a final amount in x numOfyears
      * @param monthlyOrYearlyAmount
      * @param finalAmount
@@ -32,21 +85,19 @@ public class CAGRCalculator {
      * @return
      */
     private static double calculateInterest(double monthlyOrYearlyAmount, double finalAmount, int numOfYears, boolean isMonthlyDeposit) {
-
+        // This is a wrong implementation
+        // Summation is of form Geometric Series : ar^0 + ar^1 + ar^2 + ar^3 + ...ar^n-1 = a(1-r^n)/(1-r)
         double finalAmountWithInterest = 0;
-        for (int i = 0; i < numOfYears; i++) {
-            double currYearInstallment;
-            currYearInstallment = isMonthlyDeposit ? monthlyOrYearlyAmount * 12 : monthlyOrYearlyAmount;
-            int currYearInstallmentTotalPeriod = numOfYears - i;
-            // x = Math.pow((1 + rate / 100.0), currYearInstallmentTotalPeriod) = A/P
-            //int x = currYearInstallmentFinalValueWithInterest/currYearInstallment;
-            int rate = 0;
-            double currYearInstallmentFinalValueWithInterest = currYearInstallment * (Math.pow((1 + rate / 100.0), currYearInstallmentTotalPeriod));
+        double currYearInstallment = isMonthlyDeposit ? monthlyOrYearlyAmount * 12 : monthlyOrYearlyAmount;
+        // For each year
+        // currYearInstallmentTotalPeriod = numOfYears - i
+        // A = P * Math.pow((1 + rate / 100.0), currYearInstallmentTotalPeriod)
 
-            finalAmountWithInterest += currYearInstallmentFinalValueWithInterest;
-            LOGGER.info("Year {}  Period {} Installment amount {} , Total Final Value {}", i + 1, currYearInstallmentTotalPeriod, currYearInstallment, rupeeFormat.format(currYearInstallmentFinalValueWithInterest));
-        }
-        //System.out.printf("Final Amount : %.2f\n", finalAmountWithInterest);
-        return finalAmountWithInterest;
+        // Use Geometric Summation : ar^0 + ar^1 + ar^2 + ar^3 + ...ar^n-1 = a(1-r^n)/(1-r)
+        // Final Amount = currYearInstallment
+        // double finalAmount/currYearInstallment*(r^0 + r^1 + r^2 + r^3 + ...r^n-1)
+
+
+        return 0;
     }
 }
--- src/main/java/zinterview/Sample.java
+++ src/main/java/zinterview/Sample.java
@@ -1,4 +1,30 @@
 package zinterview;
 
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Sample {
+
+    public static void main(String[] args) {
+        //String location = "https://release-condition-service-api.us-ashburn-1.oci.oc-test.com/70e73364-dd86-4f34-bd50-af8ca924655b";
+        String location = "93dc6d1d-f4b7-45ee-8b61-ed6ca891587f";
+        String[] split = location.split("/");
+        System.out.println("ID : "+split[split.length-1]);
+        Sample sample = new Sample();
+        sample.foo();
+
+    }
+
+    public void foo(){
+        String method = Thread.currentThread().getStackTrace()[1].getMethodName();
+        System.out.println(method);
+    }
+
+
+
+
 }
