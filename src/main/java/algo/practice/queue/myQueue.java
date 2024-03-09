77354c744de8c45204ca2827d442b313c5359be1
ashhhkum <dhruva.ashu@gmail.com>
2024-03-09 22:47:04 +0530

    sample changes

--- src/main/java/algo/practice/queue/myQueue.java
+++ src/main/java/algo/practice/queue/myQueue.java
@@ -1,10 +1,21 @@
 package algo.practice.queue;
 
+
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class myQueue {
     public static void main(String[] args) {
+
         Queue<Integer> queue = new LinkedList<>();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        /*
+        *
+        * add --> to add element to the queue. O(log n)
+          remove --> to get and remove the min/max. O(log n)
+          peek --> to get, but not remove the min/max. O(1)
+        * */
+
     }
 }
