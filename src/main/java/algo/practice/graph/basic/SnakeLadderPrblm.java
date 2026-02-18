4d97356b50c9543c1f4e68a5b36d64cb0d0c747e
ashhhkum <ashish.2.kumar@oracle.com>
2026-02-18 06:13:26 +0530

    snake ladder

--- src/main/java/algo/practice/graph/basic/SnakeLadderPrblm.java
+++ src/main/java/algo/practice/graph/basic/SnakeLadderPrblm.java
@@ -12,74 +12,99 @@ import org.slf4j.LoggerFactory;
  * reach the destination or last cell from the source or 1st cell.
  * Basically, the player has total control over the outcome of the dice throw.
  *
+ * This uses Breadth-First Search (BFS) to check all possible outcomes at every next step for all dice throws from 1-6.
+ * At every step, it enqueue all current cell, then check next step until it reaches last cell.
+ *
  */
 public class SnakeLadderPrblm {
     public static final Logger LOGGER = LoggerFactory.getLogger(SnakeLadderPrblm.class);
 
-    public static int minStepToReachSnakeLadderTop(int n, int[] move){
-        int[] visited = new int[n];
-        Queue<Cell> queue = new LinkedList<>();
-        Cell startCell = new Cell(0, 0);
-        visited[startCell.position] = 1;
-        queue.add(startCell);
-        Cell currentCell = new Cell();
-        while (!queue.isEmpty()){
-            currentCell = queue.poll();
-            if(currentCell.position==n-1){
+    /**
+     * Calculates the minimum number of dice throws required to reach the last cell.
+     *
+     * @param boardSize   Number of cells on the board
+     * @param cellMapping Array of size boardSize. If there's a snake or ladder at i,
+     *                    cellMapping[i] gives the destination cell. A value of 0 means normal cell.
+     * @return            Minimum number of dice throws to reach the end
+     */
+    public static int findMinDiceThrows(int boardSize, int[] cellMapping){
+        // visited[i]=true if cell i is already visited
+        boolean[] visited = new boolean[boardSize];
+
+        // Queue for BFS: stores PlayerMove states
+        Queue<PlayerMove> queue = new LinkedList<>();
+
+        // Begin the game from cell 0 with 0 dice throws
+        PlayerMove startingMove = new PlayerMove(0, 0);
+        visited[0] = true;
+        queue.add(startingMove);
+
+        PlayerMove currentMove = null;
+
+        while (!queue.isEmpty()) {
+            currentMove = queue.poll();
+
+            // Stop BFS if we've reached the last cell
+            if (currentMove.cellIndex == boardSize - 1) {
                 break;
             }
-            int currPosition = currentCell.position;
-            for(int j = currPosition+1 ; j <= currPosition+6 && j < n; j++ ){
-                //So that visited node should not be queue again.
-                if(visited[j]==0){
-                    Cell cell = new Cell();
-                    cell.dist = currentCell.dist+1;
-                    visited[j] = 1;
-                    if(move[j]!=0){
-                        //Encountered a snake/ladder
-                        cell.position = move[j];
-                    } else {
-                        cell.position = j;
-                    }
-                    queue.add(cell);
+
+            // Try moving from current cell with a dice roll of 1 to 6
+            for (int diceRoll = 1; diceRoll <= 6; diceRoll++) {
+                int nextCell = currentMove.cellIndex + diceRoll;
+                if (nextCell >= boardSize) break; // Can't go past board
+
+                if (!visited[nextCell]) {
+                    visited[nextCell] = true;
+                    //destination cell for given diceroll value factoring in snake/ladder
+                    int destination = (cellMapping[nextCell] != 0)
+                            ? cellMapping[nextCell]
+                            : nextCell;
+
+                    // Add the resulting cell and increment dice throw count
+                    PlayerMove move = new PlayerMove(destination, currentMove.diceThrows + 1);
+                    // Add to queue
+                    queue.add(move);
                 }
             }
         }
-        return currentCell.dist;
+
+        // If BFS finished and last cell was reached, return dice throws
+        return (currentMove != null) ? currentMove.diceThrows : -1;
     }
 
-    public static class Cell{
-        int position;
-        int dist;
+    /**
+     * Helper class to store player's position and the number of dice throws taken so far.
+     */
+    private static class PlayerMove {
+        int cellIndex;
+        int diceThrows;
 
-        public Cell() {
-        }
-
-        public Cell(int position, int dist) {
-            this.position = position;
-            this.dist = dist;
+        public PlayerMove(int cellIndex, int diceThrows) {
+            this.cellIndex = cellIndex;
+            this.diceThrows = diceThrows;
         }
     }
 
     public static void main(String[] args) {
-        int n = 30;
-        int[] moves = new int[n];
-        DirectedGraph directedGraph = new DirectedGraph(n);
-        for(int i = 0; i < n ; i++){
+        int boardSize = 30;
+        int[] cellMapping = new int[boardSize];
 
-        }
-        // Ladders
-        moves[2] = 21;
-        moves[4] = 7;
-        moves[10] = 25;
-        moves[19] = 28;
+        // Ladders: eg. ladder from cell 2 to 21
+        cellMapping[2] = 21;
+        cellMapping[4] = 7;
+        cellMapping[10] = 25;
+        cellMapping[19] = 28;
 
-        // Snakes
-        moves[26] = 1;
-        moves[20] = 8;
-        moves[16] = 3;
-        moves[18] = 6;
-        LOGGER.info("Minimum number to throws needed to reach from {} to  top {} of Snake Ladder Game is {}",
-                1, n, minStepToReachSnakeLadderTop(n, moves));
+        // Snakes: eg. snake from cell 26 to 1
+        cellMapping[26] = 1;
+        cellMapping[20] = 8;
+        cellMapping[16] = 3;
+        cellMapping[18] = 6;
+
+        int minThrows = findMinDiceThrows(boardSize, cellMapping);
+
+        LOGGER.info("Minimum number of dice throws needed to reach from start (cell 1) to end (cell {}) is {}",
+                boardSize, minThrows);
     }
 }
